765569e57dca2f17f32901066b7b8ae1c64567c8
Rhylanscript <181093569+Rhylanscript@users.noreply.github.com>
2026-09-09 21:25:57 +1000

    test: add tests for new opcodes in 8XY_ family

--- tests/opcodes.rs
+++ tests/opcodes.rs
@@ -1,28 +1,6 @@
 use chip8::cpu::Cpu;
 
 #[test]
-fn opcode_6xnn_sets_register() {
-    let mut cpu = Cpu::new();
-    cpu.execute(0x6A15); // sets V[A] to 0x15
-    assert_eq!(cpu.v[0xA], 0x15);
-}
-
-#[test]
-fn opcode_7xnn_adds_to_register() {
-    let mut cpu = Cpu::new();
-    cpu.v[3] = 0x10;
-    cpu.execute(0x7305); // adds 0x05 to V[3]
-    assert_eq!(cpu.v[3], 0x15);
-}
-
-#[test]
-fn opcode_annn_sets_index_register() {
-    let mut cpu = Cpu::new();
-    cpu.execute(0xA123);
-    assert_eq!(cpu.i, 0x123);
-}
-
-#[test]
 fn opcode_1nnn_jumps_pc() {
     let mut cpu = Cpu::new();
     cpu.execute(0x1300);
@@ -67,6 +45,105 @@ fn opcode_5xy0_skips_when_equal() {
 }
 
 #[test]
+fn opcode_6xnn_sets_register() {
+    let mut cpu = Cpu::new();
+    cpu.execute(0x6A15); // sets V[A] to 0x15
+    assert_eq!(cpu.v[0xA], 0x15);
+}
+
+#[test]
+fn opcode_7xnn_adds_to_register() {
+    let mut cpu = Cpu::new();
+    cpu.v[3] = 0x10;
+    cpu.execute(0x7305); // adds 0x05 to V[3]
+    assert_eq!(cpu.v[3], 0x15);
+}
+
+#[test]
+fn opcode_8xy0_copies_register() {
+    let mut cpu = Cpu::new();
+    cpu.v[2] = 0x77;
+    cpu.execute(0x8120);
+    assert_eq!(cpu.v[1], 0x77);
+}
+
+#[test]
+fn opcode_8xy4_adds_with_carry_flag() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0xFF;
+    cpu.v[1] = 0x02;
+    cpu.execute(0x8014);
+    assert_eq!(cpu.v[0], 0x01);
+    assert_eq!(cpu.v[0xF], 1);
+}
+
+#[test]
+fn opcode_8xy4_adds_without_carry_flag() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x10;
+    cpu.v[1] = 0x05;
+    cpu.execute(0x8014);
+    assert_eq!(cpu.v[0], 0x15);
+    assert_eq!(cpu.v[0xF], 0);
+}
+
+#[test]
+fn opcode_8xy5_subtracts_no_borrow() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x10;
+    cpu.v[1] = 0x05;
+    cpu.execute(0x8015);
+    assert_eq!(cpu.v[0], 0x0B);
+    assert_eq!(cpu.v[0xF], 1);
+}
+
+#[test]
+fn opcode_8xy5_subtracts_with_borrow() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x05;
+    cpu.v[1] = 0x10;
+    cpu.execute(0x8015);
+    assert_eq!(cpu.v[0xF], 0);
+}
+
+#[test]
+fn opcode_8xy6_shifts_right_and_captures_bit() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0b0000_0011;
+    cpu.execute(0x8006);
+    assert_eq!(cpu.v[0], 0b0000_0001);
+    assert_eq!(cpu.v[0xF], 1);
+}
+
+#[test]
+fn opcode_8xy7_subtracts_reversed_order() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x05;
+    cpu.v[1] = 0x10;
+    cpu.execute(0x8017);
+    assert_eq!(cpu.v[0], 0x0B);
+    assert_eq!(cpu.v[0xF], 1);
+}
+
+#[test]
+fn opcode_8xy7_subtracts_reversed_order_with_borrow() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x10;
+    cpu.v[1] = 0x05;
+    cpu.execute(0x8017);
+    assert_eq!(cpu.v[0xF], 0);
+}
+
+#[test]
+fn opcode_8xye_shifts_left_and_captures_bit() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0b1000_0001;
+    cpu.execute(0x800E);
+    assert_eq!(cpu.v[0], 0b0000_0010);
+    assert_eq!(cpu.v[0xF], 1);
+}
+
+#[test]
 fn opcode_9xy0_skips_when_not_equal() {
     let mut cpu = Cpu::new();
     cpu.v[1] = 0x20;
@@ -74,4 +151,11 @@ fn opcode_9xy0_skips_when_not_equal() {
     let pc_before = cpu.pc;
     cpu.execute(0x9120);
     assert_eq!(cpu.pc, pc_before + 4);
+}
+
+#[test]
+fn opcode_annn_sets_index_register() {
+    let mut cpu = Cpu::new();
+    cpu.execute(0xA123);
+    assert_eq!(cpu.i, 0x123);
 }
