293761173f29834c9fd3e71d89dfce707e34cad0
Rhylanscript <181093569+Rhylanscript@users.noreply.github.com>
2026-09-09 20:21:59 +1000

    test: add unit tests for new opcode skip conditions

--- tests/opcodes.rs
+++ tests/opcodes.rs
@@ -28,3 +28,50 @@ fn opcode_1nnn_jumps_pc() {
     cpu.execute(0x1300);
     assert_eq!(cpu.pc, 0x300);
 }
+
+#[test]
+fn opcode_3xnn_skips_when_equal() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x42;
+    let pc_before = cpu.pc;
+    cpu.execute(0x3042);
+    assert_eq!(cpu.pc, pc_before + 4);
+}
+
+#[test]
+fn opcode_3xnn_does_not_skip_when_not_equal() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x12;
+    let pc_before = cpu.pc;
+    cpu.execute(0x3042);
+    assert_eq!(cpu.pc, pc_before + 2);
+}
+
+#[test]
+fn opcode_4xnn_skips_when_not_equal() {
+    let mut cpu = Cpu::new();
+    cpu.v[0] = 0x12;
+    let pc_before = cpu.pc;
+    cpu.execute(0x4042);
+    assert_eq!(cpu.pc, pc_before + 4);
+}
+
+#[test]
+fn opcode_5xy0_skips_when_equal() {
+    let mut cpu = Cpu::new();
+    cpu.v[1] = 0x20;
+    cpu.v[2] = 0x20;
+    let pc_before = cpu.pc;
+    cpu.execute(0x5120);
+    assert_eq!(cpu.pc, pc_before + 4);
+}
+
+#[test]
+fn opcode_9xy0_skips_when_not_equal() {
+    let mut cpu = Cpu::new();
+    cpu.v[1] = 0x20;
+    cpu.v[2] = 0x22;
+    let pc_before = cpu.pc;
+    cpu.execute(0x9120);
+    assert_eq!(cpu.pc, pc_before + 4);
+}
